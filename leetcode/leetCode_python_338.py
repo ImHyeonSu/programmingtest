cac4c3bea137b015ce2f16a45770fab77ebaec28
ImHyeonSu <dlagustn55@naver.com>
2024-07-17 22:03:41 +0900

    2024/07/17

---
+++ leetcode/leetCode_python_338.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> list[int]:
+        result_arr = []
+        for i in range(0, n+1, 1):
+            binary = format(i, 'b')
+            count = str(binary).count("1")
+            result_arr.append(count)
+        return result_arr
+
+y = Solution()
+print(y.countBits(5))
