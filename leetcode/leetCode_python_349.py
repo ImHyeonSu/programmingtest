0b5ff27f071dabde0169f98f32bad8bf2da24059
ImHyeonSu <dlagustn55@naver.com>
2024-07-22 08:28:39 +0900

    2024/07/22

---
+++ leetcode/leetCode_python_349.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        max_list_len = max(len(nums1), len(nums2))
+        long_list = nums1
+        short_list = nums2
+        result = set()
+        if max_list_len < len(nums2):
+            long_list = nums2
+            short_list = nums1
+        for val in long_list:
+            if val in short_list:
+                result.add(val)
+        return list(result)
+
+y = Solution()
+print(y.intersection([4,9,5], [9,4,9,8,4]))
---
+++ leetcode/leetCode_python_6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        result = ""
+        str_list = []
+        process_count = 0
+        process_flag = 1
+        if numRows == 1:
+            return s
+        for i in range(numRows):
+            str_list.append([])
+        print(str_list)
+        for var_s in s:
+            if process_flag == 1 and process_count < numRows - 1:
+                str_list[process_count].append(var_s)
+                process_count += 1
+            elif process_flag == -1 and process_count > 0:
+                str_list[process_count].append(var_s)
+                process_count -= 1
+            elif process_flag == -1 and process_count == 0:
+                str_list[process_count].append(var_s)
+                process_flag = 1
+                process_count += 1
+            elif process_flag == 1 and process_count == numRows - 1:
+                str_list[process_count].append(var_s)
+                process_count -= 1
+                process_flag = -1
+        for list_var in str_list:
+            result += ''.join(list_var)
+        return result
+    
+y = Solution()
+print(y.convert("PAYPALISHIRING", 1))
