77316ab362f235fd528ca67094d33c1b412a4dab
ImHyeonSu <dlagustn55@naver.com>
2024-08-22 23:16:50 +0900

    2024/08/22

---
+++ leetcode/leetCode_python_509.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        fib_list = [0, 1]
+        if n == 0 or n == 1:
+            return fib_list[n]
+        tmp_n = 1
+        while tmp_n <= n:
+            tmp_n += 1
+            append_num = fib_list[tmp_n - 2] + fib_list[tmp_n - 1]
+            fib_list.append(append_num)
+        return fib_list[n]
+
+
+y = Solution()
+print(y.fib(4))
