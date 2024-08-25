fb657a43b811804c2a6fcdb1b7c39b8602039b90
ImHyeonSu <dlagustn55@naver.com>
2024-08-25 22:45:51 +0900

    2024/08/25

---
+++ leetcode/leetCode_python_520_ref.py
@@ -0,0 +1,9 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper() or word.islower() or word.istitle():
+            return True
+        return False
+
+
+y = Solution()
+print(y.detectCapitalUse("Google"))
---
+++ leetcode/leetCode_python_521.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        else:
+            return max(len(a),len(b))
+
+
+y = Solution()
+print(y.findLUSlength("aaa","bbb"))
