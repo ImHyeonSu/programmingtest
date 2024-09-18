1b82f49122bd549af3895733c8a5b67730f54d1d
ImHyeonSu <dlagustn55@naver.com>
2024-09-19 00:20:33 +0900

    2024/09/19-2

--- leetcode/leetCode_MYSQL_620.sql
+++ leetcode/leetCode_MYSQL_620.sql
@@ -1,3 +1,24 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- SELECT
+--     *
+-- FROM
+--     Cinema
+-- WHERE
+--     id % 2 = 1
+--     AND description != "boring"
+-- ORDER BY
+--     rating DESC
+
+
 SELECT
-    *
+    id,
+    movie,
+    description,
+    rating
+FROM
+    Cinema
+WHERE
+    id % 2 = 1
+    AND description <> 'boring'
+ORDER BY
+    rating DESC;
