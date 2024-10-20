df9993e40660d36c871e77deb2674d10090b5d93
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-10-20 11:24:16 +0900

    Time: 151 ms (95.2%), Space: 19.8 MB (85.03%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -0,0 +1,16 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.hash_map = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.hash_map[key] = value
+        return None
+
+    def get(self, key: int) -> int:
+        return self.hash_map[key] if key in self.hash_map else -1
+
+    def remove(self, key: int) -> None:
+        if key in self.hash_map:
+            del self.hash_map[key]
+        return None
