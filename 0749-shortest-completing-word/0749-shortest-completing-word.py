e7052efbcf8aafedf15abeb195916e3805baea12
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-10-20 11:15:58 +0900

    Time: 31 ms (98.63%), Space: 16.9 MB (40.47%) - LeetHub

---
+++ 0749-shortest-completing-word/0749-shortest-completing-word.py
@@ -0,0 +1,15 @@
+import re
+from collections import Counter
+
+
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: list[str]) -> str:
+        spell = "".join(re.findall(r"[a-zA-Z]", licensePlate)).lower()
+        spell_counter = Counter(spell)
+        return_str = None
+        for word in words:
+            word_counter = Counter(word.lower())
+            if all(word_counter[char] >= spell_counter[char] for char in spell_counter):
+                if return_str is None or len(word) < len(return_str):
+                    return_str = word
+        return return_str
