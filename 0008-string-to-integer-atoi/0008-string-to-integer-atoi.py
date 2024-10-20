367b686871017246ab24c0a6c01b0f92cba0e1c1
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-10-20 12:53:33 +0900

    Time: 36 ms (67.08%), Space: 16.7 MB (6.12%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,32 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if len(s) == 0:
+            return 0
+        minus_flag = False
+        tmp_list = []
+        result = 0
+        list_str = "0123456789"
+        if s[0] == "-":
+            minus_flag = True
+            s = s[1:]
+        elif s[0] == "+":
+            s = s[1:]
+        for i in s:
+            if len(tmp_list) == 0 and i == "0":
+                continue
+            if i in list_str:
+                tmp_list.append(i)
+            else:
+                break
+        tmp_result = "".join(tmp_list)
+        if tmp_result:
+            result = int(tmp_result)
+        if minus_flag:
+            result = result * -1
+        if result < -(2**31):
+            result = -(2**31)
+        elif result > 2**31 - 1:
+            result = 2**31 - 1
+
+        return result
