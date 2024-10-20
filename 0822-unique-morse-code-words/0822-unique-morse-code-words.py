e1e3a64e6e3d523e79800922c2ff6cf5dbd8a91c
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-10-20 12:38:57 +0900

    Time: 0 ms (100%), Space: 16.7 MB (11.51%) - LeetHub

---
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.py
@@ -0,0 +1,37 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: list[str]) -> int:
+        not_duplicate = set()
+        morse_code_dict = {
+            "a": ".-",
+            "b": "-...",
+            "c": "-.-.",
+            "d": "-..",
+            "e": ".",
+            "f": "..-.",
+            "g": "--.",
+            "h": "....",
+            "i": "..",
+            "j": ".---",
+            "k": "-.-",
+            "l": ".-..",
+            "m": "--",
+            "n": "-.",
+            "o": "---",
+            "p": ".--.",
+            "q": "--.-",
+            "r": ".-.",
+            "s": "...",
+            "t": "-",
+            "u": "..-",
+            "v": "...-",
+            "w": ".--",
+            "x": "-..-",
+            "y": "-.--",
+            "z": "--..",
+        }
+        for word in words:
+            morse_word = ""
+            for word_str in word:
+                morse_word += morse_code_dict[word_str]
+            not_duplicate.add(morse_word)
+        return len(not_duplicate)
