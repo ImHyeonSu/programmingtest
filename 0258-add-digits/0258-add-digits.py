de425ddf32ba7e453990328c84c477579ebf621a
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:17:47 +0900

    Time: 0 ms (100%), Space: 16.6 MB (18.39%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        # loop
+        while len(str(num)) > 1:
+            # 条件
+            return_num = 0
+            for value in str(num):
+                return_num += int(value)
+            num = return_num
+        # return
+        return num
