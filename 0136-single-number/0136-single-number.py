eb325eaa0db20546dccc9e468472a9eaebc247b3
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:51:55 +0900

    Time: 4258 ms (5.01%), Space: 18.5 MB (98.16%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+        for i in nums:
+            if nums.count(i) == 2:
+                pass
+            else:
+                return i
