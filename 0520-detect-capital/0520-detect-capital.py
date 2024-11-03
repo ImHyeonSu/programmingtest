524d34e8068124ec49ff5d2234052655d6077bda
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:33:15 +0900

    Time: 0 ms (100%), Space: 16.6 MB (55.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,32 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if len(word) == 1:
+            return True
+        word_flag = 0
+        result = True
+        if word[0].islower():
+            word_flag = 1
+        if word[0].isupper() and word[1].islower():
+            word_flag = 2
+        for index, val in enumerate(word):
+            if word_flag == 0:
+                if val.isupper():
+                    continue
+                else:
+                    result = False
+                    break
+            if word_flag == 1:
+                if val.islower():
+                    continue
+                else:
+                    result = False
+                    break
+            if word_flag == 2:
+                if index == 0:
+                    continue
+                if val.islower():
+                    continue
+                else:
+                    result = False
+                    break
+        return result
