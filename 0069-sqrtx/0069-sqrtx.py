636ee8208fc598dd22a715c684ac5c778e25da1c
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:42:28 +0900

    Time: 1066 ms (13.33%), Space: 16.6 MB (16.02%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        result = 0
+        mulDate = 0
+        while mulDate <= x:
+            result += 1
+            mulDate = result * result
+
+        if mulDate > x:
+            return result - 1
+        elif mulDate == x:
+            return result
