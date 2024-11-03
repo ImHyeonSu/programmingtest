1de8caf33a2d7575ab1a9449499df2b9ff74a7b6
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:50:39 +0900

    Time: 0 ms (100%), Space: 16.7 MB (16.13%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, numRows: int) -> list[list[int]]:
+        result = []
+        for i in range(1, 2 + numRows, 1):
+            ins_arr = []
+            for ins_i in range(1, 1 + i, 1):
+                if ins_i == 1 or ins_i == i:
+                    ins_arr.append(1)
+                else:
+                    ins_arr.append(result[i - 2][ins_i - 2] + result[i - 2][ins_i-1])
+            result.append(ins_arr)
+        return result[numRows]
