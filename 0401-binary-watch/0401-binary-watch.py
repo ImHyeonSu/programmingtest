b41f1d862b2c8bb0bf74baac30c0fb7ffe108dbe
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:25:24 +0900

    Time: 4 ms (14.9%), Space: 16.7 MB (34.41%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,8 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> list[str]:
+        result = []
+        for h in range(12):
+            for m in range(60):
+                if bin(h).count("1") + bin(m).count("1") == turnedOn:
+                    result.append(f"{h}:{m:02d}")
+        return result
