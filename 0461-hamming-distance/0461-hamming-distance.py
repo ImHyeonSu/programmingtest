3b9ba0e6e0f25b76e14937ccfeb8bf4841d7c899
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:28:49 +0900

    Time: 0 ms (100%), Space: 16.7 MB (17.46%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        binary_x = format(x, "032b")
+        binary_y = format(y, "032b")
+        result = 0
+        for i in range(len(binary_x)):
+            if binary_x[i] != binary_y[i]:
+                result += 1
+        return result
