89eccef42b9137653e16dd076f8b18f42120050f
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 03:05:13 +0900

    Time: 34 ms (15.04%), Space: 22.9 MB (10.66%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -14,16 +14,3 @@ class KthLargest:
             heappop(self.min_heap)
         return self.min_heap[0]
 
-
-commands = ["KthLargest", "add", "add", "add", "add", "add"]
-values = [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
-
-result = []
-
-kth_largest = KthLargest(values[0][0], values[0][1])
-result.append(None)
-
-
-for i in range(1, len(commands)):
-    result.append(kth_largest.add(values[i][0]))
-print(result)
