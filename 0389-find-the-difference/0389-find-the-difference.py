52c3907e3d4cb2f01aa8298a169cf06ba520ec08
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:24:57 +0900

    Time: 3 ms (55.39%), Space: 16.7 MB (33.04%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+
+        counter1 = Counter(list(s))
+        counter2 = Counter(list(t))
+
+        diff = counter2 - counter1
+        return list(diff.elements())[0]
