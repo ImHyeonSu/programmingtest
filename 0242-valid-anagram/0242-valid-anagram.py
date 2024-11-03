4f016c3ee767b2f72c72ba9357c9582b575a010c
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:17:29 +0900

    Time: 13 ms (56.4%), Space: 16.6 MB (99.53%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dic = dict()
+        t_dic = dict()
+        for i in s:
+            if i in s_dic:
+                s_dic[i] = s_dic[i] +1
+            else:
+                s_dic[i] = 1
+        for i in t:
+            if i in t_dic:
+                t_dic[i] = t_dic[i] +1
+            else:
+                t_dic[i] = 1        # for index in t:
+        if(s_dic == t_dic):
+            return True
+        return False
