ab52ce0cfb514575183f840dff5b643941e0d646
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:53:53 +0900

    Time: 3 ms (17.89%), Space: 16.6 MB (45.39%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,15 @@
+import string
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        upper_list = list(string.ascii_uppercase);
+        
+        title_len = len(columnTitle)
+        
+        if title_len == 1:
+            return upper_list.index(columnTitle)+1
+        result = 0
+
+        for index,value in enumerate(columnTitle):
+            result += (upper_list.index(value)+1) * 26 **(title_len-index-1)
+        return result
