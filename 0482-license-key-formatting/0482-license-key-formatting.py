57430a58f8c0cafb8c70d68d2fc6fbb0f0cb521e
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:29:34 +0900

    Time: 3 ms (92.09%), Space: 17 MB (97.61%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.py
@@ -0,0 +1,15 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        replace_s = s.replace("-", "")
+        result = ""
+        if len(replace_s) % k == 0:
+            first_len = 0
+        else:
+            first_len = len(replace_s) % k
+            result += replace_s[0:first_len]
+
+        for i in range(first_len, len(replace_s), k):
+            if i != 0:
+                result += "-"
+            result += replace_s[i : i + k]
+        return result.upper()
