7223a99de74febfad31acd708e4b62c921d1f8e1
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:25:12 +0900

    Time: 0 ms (100%), Space: 16.7 MB (36.93%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        index = 0
+        plus_num = 0
+        for i in s:
+            if t.find(i, index + plus_num) != -1 and index <= t.find(
+                i, index + plus_num
+            ):
+                index = t.find(i, index + plus_num)
+                plus_num = 1
+            else:
+                return False
+
+        return True
