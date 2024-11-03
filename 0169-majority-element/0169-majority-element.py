140283759d17f03f4e41a7989629a1534c17ec7a
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 02:53:28 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        resultdic = {}
+        for val in nums:
+            if not val in resultdic:
+                resultdic[val] = 1
+            else:
+                resultdic[val] = resultdic[val] + 1
+        print(max(resultdic,key=resultdic.get));
