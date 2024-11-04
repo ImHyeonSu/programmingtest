4b96e29822922bdba572ca8ed0852055ec484395
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2024-11-04 21:10:37 +0900

    Time: 59 ms (5.93%), Space: 16.8 MB (7.48%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        for row_i in range(0, 9, 1):
+            # check row
+            count_num = Counter(board[row_i])
+            del count_num["."]
+            if any(count >= 2 for count in count_num.values()):
+                return False
+            # check column
+            for i in range(0, 9, 1):
+                for j in range(0, 9, 1):
+                    for z in range(i + 1, 9, 1):
+                        if board[i][j] == "." or board[z][j] == ".":
+                            continue
+                        if board[i][j] == board[z][j]:
+                            return False
+            # check box
+            for i in range(0, 3, 1):
+                box_list = []
+                for j in range(0, 3, 1):
+                    box_list.append(board[i * 3][j * 3])
+                    box_list.append(board[i * 3][j * 3 + 1])
+                    box_list.append(board[i * 3][j * 3 + 2])
+                    for z in range(i * 3 + 1, i * 3 + 3, 1):
+                        box_list.append(board[z][j * 3])
+                        box_list.append(board[z][j * 3 + 1])
+                        box_list.append(board[z][j * 3 + 2])
+                    count_num = Counter(box_list)
+                    del count_num["."]
+                    if any(count >= 2 for count in count_num.values()):
+                        return False
+                    box_list = []
+        return True
