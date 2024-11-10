995348e68a6ae069cecc9927e7f2e3488543e6f3
tfp-Im <h.im@tfp-group.co.jp>
2024-11-10 20:25:27 +0900

    2024/11/10_2

--- leetcode/leetCode_python_884.py
+++ leetcode/leetCode_python_884.py
@@ -4,12 +4,10 @@ class Solution:
         split_s1 = s1.split(" ")
         split_s2 = s2.split(" ")
         for tmp_s1 in split_s1:
-            print(tmp_s1, split_s1.count(tmp_s1), tmp_s1 in split_s2)
             if split_s1.count(tmp_s1) > 1 or tmp_s1 in split_s2:
                 continue
             result.append(tmp_s1)
         for tmp_s2 in split_s2:
-            print(tmp_s2, split_s2.count(tmp_s2), tmp_s2 in split_s1)
             if split_s2.count(tmp_s2) > 1 or tmp_s2 in split_s1:
                 continue
             result.append(tmp_s2)
