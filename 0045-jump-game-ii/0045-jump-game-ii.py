f0fb80fc10d2fd2372182590002655d4db998de4
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2025-04-13 23:03:33 +0900

    Time: 11 ms (44.18%), Space: 18.7 MB (55.63%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return 0
+        jumps = 0
+        current_max_reach = 0
+        next_max_reach = 0
+        for idx in range(n - 1):
+            next_max_reach = max(next_max_reach, nums[idx] + idx)
+            if idx == current_max_reach:
+                jumps += 1
+                current_max_reach = next_max_reach
+            if current_max_reach >= n - 1:
+                break
+
+        return jumps
