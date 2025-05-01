9b0368bdbfb259c9286bf0c70cee39d928a685fe
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2025-05-01 23:01:35 +0900

    Time: 3 ms (66.36%), Space: 17.9 MB (27.1%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last_pos = {}
+        for i, char in enumerate(s):
+            last_pos[char] = i
+        print(last_pos)
+        result = []
+        seen = set()
+        for i, char in enumerate(s):
+            if char in seen:
+                continue
+            while result and char < result[-1] and i < last_pos[result[-1]]:
+                seen.remove(result.pop())
+            result.append(char)
+            seen.add(char)
+        return "".join(result)
