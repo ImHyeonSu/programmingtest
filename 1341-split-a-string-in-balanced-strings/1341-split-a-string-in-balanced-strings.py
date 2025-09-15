4632bbd1594ed42a5371a3d0927007539885cc7b
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2025-09-16 06:57:42 +0900

    Time: 0 ms (100%), Space: 17.9 MB (7.79%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count = 0
+        s_count = 0
+        for v in s:
+            if v == "R":
+                s_count += 1
+            elif v == "L":
+                s_count -= 1
+            if s_count == 0:
+                count += 1
+        return count
