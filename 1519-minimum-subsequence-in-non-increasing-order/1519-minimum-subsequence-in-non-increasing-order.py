226b32237a8e79f50083865550d6e77d5589fce5
h2yn <57281748+ImHyeonSu@users.noreply.github.com>
2025-09-18 07:28:38 +0900

    Time: 19 ms (11.48%), Space: 17.7 MB (59.59%) - LeetHub

--- 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
+++ 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
@@ -1,6 +1,5 @@
 class Solution:
     def minSubsequence(self, nums: list[int]) -> list[int]:
-        numbers = 0
         nums.sort(reverse=True)
         for i in range(len(nums)):
             numbers = i
